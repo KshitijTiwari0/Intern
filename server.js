89869748e225cbe8b3ad944094e44ef710f95770
KshitijTiwari0 <satyarupkshitij3002@gmail.com>
2023-07-21 13:45:58 +0530

    Version 2: Your description here

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const payDetails = [];
 // Define routes
 app.get('/', (req, res) => {
   res.render('index');
@@ -20,19 +19,25 @@ app.get("/winner", (req, res) => {
 });
 
 app.get("/smoker", (req, res) => {
-    res.render("smoker");
-  });
-  
-app.get("/about", (req, res) => {
-    res.render("about");
-});
-  
-app.get("/payment", (req, res) => {
-    res.render("payment");
+  res.render("smoker");
 });
 
-app.get("/invoice", (req, res) => {
-    res.render("invoice");
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/payment", (req, res) => {
+  res.render("payment");
+});
+
+// Handle POST request for the /payment route
+app.post("/payment", (req, res) => {
+  // Process the payment details and create an invoice if needed
+  // You can access the form data from req.body and perform necessary actions
+  const { name, email, address, paymentMethod } = req.body;
+
+  // You can pass the data to the invoice template and render it
+  res.render("invoice", { name, email, address, paymentMethod });
 });
 
 
